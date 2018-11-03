5573d179e0a27437d547c271c18be792122d7904
lulu19870526 <www.lovelulu.net@163.com>
2018-11-03 11:40:32 +0800

    SimpleDateFormat 线程不安全 解决方案

---
+++ src/main/java/javaStudy/DateUtil1.java
@@ -0,0 +1,18 @@
+package javaStudy;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateUtil1 {
+
+    public static  String formatDate(Date date)throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        return sdf.format(date);
+    }
+
+    public static Date parse(String strDate) throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        return sdf.parse(strDate);
+    }
+}
