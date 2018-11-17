02ad9d2f84ac5af08f7a66b49b781a7190cebbaa
lulu19870526 <www.lovelulu.net@163.com>
2018-11-17 16:34:18 +0800

    CountDownLatch

---
+++ src/main/java/thread/CountDownLatchTest.java
@@ -0,0 +1,47 @@
+package thread;
+
+import java.util.concurrent.CountDownLatch;
+
+public class CountDownLatchTest {
+
+    public static void main(String[] args) {
+        final CountDownLatch latch = new CountDownLatch(2);
+
+        new Thread(){
+            public void run() {
+                try {
+                    System.out.println("子线程"+Thread.currentThread().getName()+"正在执行");
+                    Thread.sleep(3000);
+                    System.out.println("子线程"+Thread.currentThread().getName()+"执行完毕");
+                    //将count值减1
+                    latch.countDown();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            };
+        }.start();
+
+        new Thread(){
+            public void run() {
+                try {
+                    System.out.println("子线程"+Thread.currentThread().getName()+"正在执行");
+                    Thread.sleep(3000);
+                    System.out.println("子线程"+Thread.currentThread().getName()+"执行完毕");
+                    latch.countDown();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            };
+        }.start();
+
+        try {
+            System.out.println("等待2个子线程执行完毕...");
+            //调用await()方法的线程会被挂起，它会等待直到count值为0才继续执行
+            latch.await();
+            System.out.println("2个子线程已经执行完毕");
+            System.out.println("继续执行主线程");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
