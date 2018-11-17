62c803d057d71dad3b32fde295399e441f142962
lulu19870526 <www.lovelulu.net@163.com>
2018-11-17 15:20:38 +0800

    排序

---
+++ src/main/java/sort/SelectSort.java
@@ -0,0 +1,42 @@
+package sort;
+
+public class SelectSort {
+
+    public static void main(String[] args){
+        int arr[] = {1,4,6,8,2,5,3,7,9};
+        System.out.println("数组排序前顺序：");
+        for(int n : arr){
+            System.out.print(n+" ");
+        }
+
+
+        sort(arr);
+
+        System.out.println("\n数组排序后顺序：");
+        for(int n : arr){
+            System.out.print(n+" ");
+        }
+
+    }
+    public static void sort(int[] R){
+        int i,j,k;
+        int temp;
+        for(i=0;i<R.length;i++){
+            k=i;//k表示最小的元素的坐标；
+            //在无序序列中找到最小的元素
+            for(j=i+1;j<R.length;j++){
+                if(R[k]>R[j]){
+                    k=j;
+                }
+            }
+
+            if(i != k){
+                //最小元素和无序序列的第一个元素交换
+                temp = R[k];
+                R[k]=R[i];
+                R[i]=temp;
+            }
+
+        }
+    }
+}
