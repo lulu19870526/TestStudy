0ff2faca3f007a16d572adea0b9883433ee7194c
lulu19870526 <www.lovelulu.net@163.com>
2018-11-18 21:25:18 +0800

    适配器模式

---
+++ src/main/java/base/adapter/MediaPlayer.java
@@ -0,0 +1,6 @@
+package base.adapter;
+
+public interface MediaPlayer {
+
+    public void play(String audioType, String fileName);
+}
---
+++ src/main/java/base/adapter/VlcPlayer.java
@@ -0,0 +1,11 @@
+package base.adapter;
+
+public class VlcPlayer  implements AdvancedMediaPlayer{
+    public void playVlc(String fileName) {
+        System.out.println("Playing vlc file. Name: "+ fileName);
+    }
+
+    public void playMp4(String fileName) {
+        //什么也不做
+    }
+}
