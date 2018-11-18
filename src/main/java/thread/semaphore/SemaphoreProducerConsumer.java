955af65a330c13090ec05914be02f59c33ad78b0
lulu19870526 <www.lovelulu.net@163.com>
2018-11-18 17:04:33 +0800

    semaphore

---
+++ src/main/java/thread/semaphore/SemaphoreProducerConsumer.java
@@ -0,0 +1,79 @@
+package thread.semaphore;
+
+import java.util.concurrent.Semaphore;
+
+public class SemaphoreProducerConsumer {
+
+    //初始容量
+    private static final int N = 10;
+
+    /***
+     * full 产品容量
+     * empty 空余容量
+     * mutex 读写锁
+     */
+    private static Semaphore full,empty,mutex;
+    //记录当前的产品数量
+    private static volatile int count = 0 ;
+
+    static {
+        /**
+         * full 初始化0个产品
+         * empty 初始化有N个空余位置放置产品
+         * mutex 初始化每次最多只有一个线程可以读写
+         * */
+        full = new Semaphore(0);
+        empty = new Semaphore(N);
+        mutex = new Semaphore(1);
+    }
+
+
+    public static void main(String []args){
+        //生产线线程
+        new Thread(new Producer()).start();
+        //消费者线程
+        new Thread(new Consumer()).start();
+    }
+
+    //生产者类
+    static class Producer implements Runnable{
+
+        public void run() {
+            while (true){
+                try {
+                    empty.acquire();//等待空位
+                    mutex.acquire();//等待读写锁
+                    count++;
+                    System.out.println("生产者生产了一个，count="+count);
+                    mutex.release();//释放读写锁
+                    full.release();//放置产品
+                    //随机休息一段时间，让生产者线程有机会抢占读写锁
+                    Thread.sleep(((int)Math.random())%10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    //消费者类
+    static class Consumer implements Runnable{
+
+        public void run() {
+            while (true){
+                try {
+                    full.acquire();//等待产品
+                    mutex.acquire();//等待读写锁
+                    count--;
+                    System.out.println("消费者消费了一个，count="+count);
+                    mutex.release();//释放读写锁
+                    empty.release();//释放空位
+                    //随机休息一段时间，让消费者线程有机会抢占读写锁
+                    Thread.sleep(((int)Math.random())%10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/thread/semaphore/SemaphoreTest.java
@@ -0,0 +1,42 @@
+package thread.semaphore;
+
+import java.util.concurrent.Semaphore;
+
+/**
+ * 假若一个工厂有5台机器，
+ * 但是有8个工人，一台机器同时只能被一个工人使用，只有使用完了，其他工人才能继续使用
+ */
+public class SemaphoreTest {
+
+    public static void main(String[] args) {
+        int N = 8;            //工人数
+        //Semaphore可以控同时访问的线程个数
+        Semaphore semaphore = new Semaphore(5); //机器数目
+        for(int i=0;i<N;i++)
+            new Worker(i,semaphore).start();
+    }
+
+    static class Worker extends Thread{
+        private int num;
+        private Semaphore semaphore;
+        public Worker(int num,Semaphore semaphore){
+            this.num = num;
+            this.semaphore = semaphore;
+        }
+
+        @Override
+        public void run() {
+            try {
+                //acquire()用来获取一个许可，若无许可能够获得，则会一直等待，直到获得许可。
+                semaphore.acquire();
+                System.out.println("工人"+this.num+"占用一个机器在生产...");
+                Thread.sleep(2000);
+                System.out.println("工人"+this.num+"释放出机器");
+                //release()用来释放许可
+                semaphore.release();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
