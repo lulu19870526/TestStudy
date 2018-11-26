8fc118946251fcbba043da9bdf218ad514333591
lulu19870526 <www.lovelulu.net@163.com>
2018-11-26 22:25:30 +0800

    快速排序：通过一趟排序将待排记录分割成两部分，其中一部分记录的关键字比另一部分记录的关键字小

--- src/main/java/sort/QuicklySort.java
+++ src/main/java/sort/QuicklySort.java
@@ -1,7 +1,7 @@
 package sort;
 
 public class QuicklySort {
-
+    //通过一趟排序将待排记录分割成两部分，其中一部分记录的关键字比另一部分记录的关键字小
     private static int getMiddle(Integer[] list, int low, int high) {
         int tmp = list[low];    //数组的第一个作为中轴
         while (low < high) {
@@ -13,11 +13,10 @@ public class QuicklySort {
                 low++;
             }
             list[high] = list[low];   //比中轴大的记录移到高端
-        }
+        }//最终结束：low=high
         list[low] = tmp;              //中轴记录到尾
         return low;                   //返回中轴的位置
     }
-
     private static void quickSort(Integer[] list, int low, int high) {
         if (low < high) {
             int middle = getMiddle(list, low, high);  //将list数组进行一分为二
