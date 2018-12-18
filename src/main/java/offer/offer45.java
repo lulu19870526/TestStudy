15d3f6d1948f014685f369c05065a439ee0d43b7
lulu19870526 <www.lovelulu.net@163.com>
2018-12-18 15:42:39 +0800

    圆圈中最后剩下的数字

---
+++ src/main/java/offer/offer45.java
@@ -0,0 +1,41 @@
+package offer;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class offer45 {
+
+    public static void main(String[] args){
+        int remain = remainLast(5,3);
+        System.out.println("remain="+remain);
+    }
+    public static int remainLast(int n,int m){
+        if(n <= 0 || m <=0 )
+            return -1;
+        List<Integer> list = new ArrayList<Integer>();
+        for(int i=0;i<n;i++)
+            list.add(i);
+
+        int currentSize = list.size();
+        Iterator<Integer> iterator = list.iterator();
+        while(currentSize >1){
+            for(int i=0;i<m;i++)
+                if(iterator.hasNext())
+                {
+                    int temp = iterator.next();
+                    //System.out.println("temp="+temp);
+                }else{
+                    iterator = list.iterator();
+                    int temp = iterator.next();
+                   // System.out.println("qqq temp="+temp);
+                }
+
+               iterator.remove();
+                currentSize --;
+        }
+        iterator = list.iterator();
+        return iterator.next();
+
+    }
+}
