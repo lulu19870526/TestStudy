dbbae2f63664eb8aea873a5db5102b4e0fe4ba5e
lulu19870526 <www.lovelulu.net@163.com>
2018-12-18 15:59:02 +0800

    剑指offer45

--- src/main/java/offer/offer45.java
+++ src/main/java/offer/offer45.java
@@ -9,7 +9,20 @@ public class offer45 {
     public static void main(String[] args){
         int remain = remainLast(5,3);
         System.out.println("remain="+remain);
+        int remain1 = remainLast1(5,3);
+        System.out.println("remain1="+remain1);
     }
+
+    public static int remainLast1(int n,int m){
+        int start = 0;
+        for(int i=2;i<= n;i++){
+            start = (start +m)%i;
+        }
+        return start;
+    }
+
+
+
     public static int remainLast(int n,int m){
         if(n <= 0 || m <=0 )
             return -1;
